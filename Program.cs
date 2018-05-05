1c328ec3225c0f625c5086c79145b047415280b3
Maksym Havryliuk <maks.havryliuk@gmail.com>
2018-05-06 00:33:33 +0300

    Threads, the solution of the 3rd task.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+using System.IO;
+
+/**<remark>
+ * Write 10 numbers and 10 strings to the same file. Numbers are written from one thread, strings from another one.
+ * Запишите 10 чисел и 10 строк в один и тот же файл. Числа записываются из одного потока, строки из другого.
+</remark> */
+
+namespace _20180329_Task3_Thread
+{
+    internal class Program
+    {
+        private static readonly StreamWriter commonResource = new StreamWriter(@"CommonFile.txt", false);
+
+        private static void FirstTread()
+        {
+            Console.WriteLine("Первый поток начал запись!");
+
+            for (int i = 1; i <= 10; i++)
+            {
+                commonResource.WriteLine(i);
+            }
+            Console.WriteLine("Первый поток завершил запись!");
+        }
+
+        private static void SecondTread()
+        {
+            Console.WriteLine("Второй поток начал запись!");
+
+            for (int i = 1; i <= 10; i++)
+            {
+                commonResource.WriteLine("Hello User!");
+            }
+            Console.WriteLine("Второй поток завершил запись!");
+        }
+
+        private static void Main()
+        {
+            Thread firstThread = new Thread(FirstTread);
+            Thread secondThread = new Thread(SecondTread);
+
+            firstThread.Start();
+            secondThread.Start();
+
+            firstThread.Join();
+            secondThread.Join();
+
+            commonResource.Close();
+
+            Console.ReadKey();
+        }
+    }
+}
